cda87f8c3c124e6f7412eb484fa9e7c16c0d4116
Mihai Gabriel <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-17 14:20:19 +0300

    Set up our stylesheet and wrapped up the chatapp using pusher api key

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -2,39 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
+use App\Events\PusherBroadcast;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-use App\Events\PusherBroadcast;
 
 class PusherController extends Controller
 {
-    /**
-     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
-     */
-    public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    public function index()
     {
         return view('index');
     }
 
-    /**
-     * @param $request
-     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
-     */
-    public function broadcast($request): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    public function broadcast(Request $request)
     {
         broadcast(new PusherBroadcast($request->get('message')))->toOthers();
 
         return view('broadcast', ['message' => $request->get('message')]);
     }
 
-    /**
-     * @param $request
-     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
-     */
-    public function receive($request): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    public function receive(Request $request)
     {
         return view('receive', ['message' => $request->get('message')]);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,89 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Realtime Laravel Chat</title>
+    <title>Chat Laravel Pusher</title>
+    <link rel="icon" href="https://assets.edlin.app/favicon/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- JavaScript -->
     <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- End JavaScript -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/style.css">
+    <!-- End CSS -->
+
 </head>
+
 <body>
+<div class="chat">
 
-<div class="top">
-    <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar">
-    <div>
-        <p>John Snow</p>
-        <small>Online</small>
+    <!-- Header -->
+    <div class="top">
+        <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar">
+        <div>
+            <p>John Snow</p>
+            <small>Online</small>
+        </div>
     </div>
-</div>
+    <!-- End Header -->
 
-<div class="messages">
-    @include('receive', ['message' => 'Hey! Whats up! #NBSP'])
-</div>
+    <!-- Chat -->
+    <div class="messages">
+        @include('receive', ['message' => "Hey! What's up!  👋"])
+        @include('receive', ['message' => "Ask a friend to open this link and you can chat with them!"])
+    </div>
+    <!-- End Chat -->
 
-<div class="bottom">
-    <form>
-        <input type="text" name="message" id="message" placeholder="Enter message..." autocomplete="off">
-        <button type="submit"></button>
-    </form>
-</div>
+    <!-- Footer -->
+    <div class="bottom">
+        <form>
+            <input type="text" id="message" name="message" placeholder="Enter message..." autocomplete="off">
+            <button type="submit"></button>
+        </form>
+    </div>
+    <!-- End Footer -->
 
+</div>
 </body>
+
+<script>
+    const pusher = new Pusher('{{config('broadcasting.connections.pusher.key')}}', {cluster: 'eu'});
+    const channel = pusher.subscribe('public');
+
+    //Receive messages
+    channel.bind('chat', function (data) {
+        $.post("/receive", {
+            _token: '{{csrf_token()}}',
+            message: data.message,
+        })
+            .done(function (res) {
+                $(".messages > .message").last().after(res);
+                $(document).scrollTop($(document).height());
+            });
+    });
+
+    //Broadcast messages
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "/broadcast",
+            method: 'POST',
+            headers: {
+                'X-Socket-Id': pusher.connection.socket_id
+            },
+            data: {
+                _token: '{{csrf_token()}}',
+                message: $("form #message").val(),
+            }
+        }).done(function (res) {
+            $(".messages > .message").last().after(res);
+            $("form #message").val('');
+            $(document).scrollTop($(document).height());
+        });
+    });
+
+</script>
 </html>
