ef27675a8dcc33d38d7fcd2939fce9f5c30ac4f4
Mihai Gabriel <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-17 13:52:23 +0300

    basic index blade set up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Realtime Laravel Chat</title>
+
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body>
+
+<div class="top">
+    <img src="https://assets.edlin.app/images/rossedlin/03/rossedlin-03-100.jpg" alt="Avatar">
+    <div>
+        <p>John Snow</p>
+        <small>Online</small>
+    </div>
+</div>
+
+<div class="messages">
+    @include('receive', ['message' => 'Hey! Whats up! #NBSP'])
+</div>
+
+<div class="bottom">
+    <form>
+        <input type="text" name="message" id="message" placeholder="Enter message..." autocomplete="off">
+        <button type="submit"></button>
+    </form>
+</div>
 
 </body>
 </html>
