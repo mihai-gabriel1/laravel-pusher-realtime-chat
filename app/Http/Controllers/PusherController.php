adc08f548a5c3c8c135ea685c914a4476c35c3d1
Mihai Gabriel <98354641+mihai-gabriel1@users.noreply.github.com>
2023-08-17 13:36:13 +0300

    Added our methods in the pusher controller

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class PusherController extends Controller
 {
-    //
+    /**
+     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+     */
+    public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('index');
+    }
+
+    /**
+     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+     */
+    public function broadcast(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('broadcast');
+    }
+
+    /**
+     * @return View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+     */
+    public function receive(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('receive');
+    }
 }
